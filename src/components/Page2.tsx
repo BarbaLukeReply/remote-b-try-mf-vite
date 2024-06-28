9e56088cbea09afee5afbe86426fcedff67357c0
l.barbagallo <l.barbagallo@tecmasolutions.com>
2024-06-28 12:32:00 +0200

    chore: Add @barbalukereply/storybooktrycomponentlib as a dependency

--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@barbalukereply/storybooktrycomponentlib"
 const Page2 = () => {
 
   return (
     <div className="flex items-center justify-center flex-grow bg-red-500 bg-opacity-50">
       <h1>Remote Component B</h1>
+      <Button text="Button from storybook remote lib in github" />
     </div>
   );
 };
