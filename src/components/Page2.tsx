400f63db1648b155b6c9b160255482ffd3f94437
l.barbagallo <l.barbagallo@tecmasolutions.com>
2024-06-28 12:56:47 +0200

    feat: Update Page2 component styling and add primary button

--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -2,9 +2,9 @@ import { Button } from "@barbalukereply/storybooktrycomponentlib"
 const Page2 = () => {
 
   return (
-    <div className="flex items-center justify-center flex-grow bg-red-500 bg-opacity-50">
+    <div className="flex flex-col items-center justify-center flex-grow bg-green-500 bg-opacity-50 space-y-4 p-4">
       <h1>Remote Component B</h1>
-      <Button text="Button from storybook remote lib in github" />
+      <Button text="Button from storybook remote lib in github" primary />
     </div>
   );
 };
