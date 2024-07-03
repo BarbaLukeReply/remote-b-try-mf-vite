3bbfcc88217b22d9dd4dd490f754e4f0704ccad4
l.barbagallo <l.barbagallo@tecmasolutions.com>
2024-07-03 15:13:34 +0200

    chore: update click for context alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
-import Page2 from './components/Page2'
+import React from 'react';
+import './App.css';
+import Page2 from './components/Page2';
 
-function App() {
-
-  return (
-    <Page2 />
-  )
+interface AlertType {
+  id: string;
+  message: string;
+  severity: "info" | "warning" | "error" | "success";
+  timeout?: number;
 }
 
-export default App
+interface UseAlertsType {
+  addAlert: (alert: Omit<AlertType, 'id'>) => void;
+  clearAlerts: () => void;
+}
+
+interface AppProps {
+  useAlerts?: () => UseAlertsType;
+}
+
+const App: React.FC<AppProps> = ({ useAlerts }) => {
+  return <Page2 useAlerts={useAlerts} />;
+};
+
+export default App;
--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -1,10 +1,43 @@
-import { Button } from "@barbalukereply/storybooktrycomponentlib"
-const Page2 = () => {
+// Importazioni
+import React, { useCallback } from "react";
+import { Button } from "@barbalukereply/storybooktrycomponentlib";
+interface AlertType {
+  id: string;
+  message: string;
+  severity: "info" | "warning" | "error" | "success";
+  timeout?: number;
+}
+
+interface UseAlertsType {
+  addAlert: (alert: Omit<AlertType, 'id'>) => void;
+  clearAlerts: () => void;
+}
+interface ComponentProps {
+  useAlerts?: () => UseAlertsType;
+}
+
+const Page2: React.FC<ComponentProps> = ({ useAlerts }) => {
+  const { addAlert } = useAlerts?.() || {};
+  
+
+  const handleTryEvents = useCallback(() => {
+    console.log("Button pressed & event sent!");
+    const initialFormData: AlertType = {
+      severity: "warning", message: "", timeout: 0,
+      id: ""
+    };
+    window.dispatchEvent(new CustomEvent("TRY_EVENT", { detail: "ci sono!" }));
+    addAlert?.({ ...initialFormData, message: "This is an alert message in Warning Type called from remote component but using context from the shell app" });
+  }, [addAlert]);
 
   return (
     <div className="flex flex-col items-center justify-center flex-grow bg-green-500 bg-opacity-50 space-y-4 p-4">
       <h1>Remote Component B</h1>
-      <Button text="Button from storybook remote lib in github" primary />
+      <Button
+        text="Button from storybook remote lib in github"
+        primary
+        onClick={handleTryEvents}
+      />
     </div>
   );
 };
